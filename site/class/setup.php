d0f22fe257da53946b8e0a6615d32098002b6bef
Jhonatan Luiz <jonatapqt01@gmail.com>
2019-05-24 12:58:46 -0300

    +bugfix

--- site/class/setup.php
+++ site/class/setup.php
@@ -8,8 +8,8 @@ class Setup
         $status = $cvbd->loginStatus();
         $cvnome = $status[1];
         $status = $status[0];
-      
-        
+
+
         ?>
     <!-- Navigation -->
     <nav class="navmenu navbar navbar-expand-lg navbar-dark fixed-top">
@@ -23,26 +23,28 @@ class Setup
             </button>
             <div class="collapse navbar-collapse" id="navbarResponsive">
                 <ul class="navbar-nav ml-auto">
-                  
+
                     <li class="nav-item">
                         <a class="nav-link active" href="#">Sobre</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link active" href="#">Contato</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link active" href="#">Reservas</a>
-                    </li>
+                    <?php
+                    if ($status) {
+                        echo "<li class='nav-item active'><a class='nav-link active' href='reservas.php'>Reservas</a></li>";
+                    }
+                    ?>
 
                     <li class="nav-item dropdown no-arrow">
                         <a class="nav-link dropdown-toggle <?php if ($status) echo "active" ?>" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <?= $cvnome ?>  
-                        <i class="fas fa-user-circle fa-fw "></i>
+                            <?= $cvnome ?>
+                            <i class="fas fa-user-circle fa-fw "></i>
                         </a>
                         <div class="dropdown-menu dropdown-menu-right" aria-labelledby="userDropdown">
                             <?php
                             if ($status) {
-                               
+
                                 echo "<a class='dropdown-item' href='user.php?cv=configurar'>Configurar</a>";
                                 echo "<a class='dropdown-item' href='login.php?login=nao'>Sair</a>";
                             } else {
@@ -130,77 +132,77 @@ public function slide($images, $nome)
 <?php
 }
 public function galeria()
- {
+{
     ?>
-<div>
-    <div class="container">
-      <div class="slideshow">
-        <h2 style="text-align:center; font-weight: bold">Slideshow Gallery</h2>
+    <div>
+        <div class="container">
+            <div class="slideshow">
+                <h2 style="text-align:center; font-weight: bold">Slideshow Gallery</h2>
 
-        <div class="slideimg">
-          <div class="mySlides">
-            <div class="numbertext">1 / 6</div>
-            <img src="galeria/img_woods_wide.jpg" style="width:100%">
-          </div>
+                <div class="slideimg">
+                    <div class="mySlides">
+                        <div class="numbertext">1 / 6</div>
+                        <img src="galeria/img_woods_wide.jpg" style="width:100%">
+                    </div>
 
-          <div class="mySlides">
-            <div class="numbertext">2 / 6</div>
-            <img src="galeria/img_5terre_wide.jpg" style="width:100%">
-          </div>
+                    <div class="mySlides">
+                        <div class="numbertext">2 / 6</div>
+                        <img src="galeria/img_5terre_wide.jpg" style="width:100%">
+                    </div>
 
-          <div class="mySlides">
-            <div class="numbertext">3 / 6</div>
-            <img src="galeria/img_mountains_wide.jpg" style="width:100%">
-          </div>
+                    <div class="mySlides">
+                        <div class="numbertext">3 / 6</div>
+                        <img src="galeria/img_mountains_wide.jpg" style="width:100%">
+                    </div>
 
-          <div class="mySlides">
-            <div class="numbertext">4 / 6</div>
-            <img src="galeria/img_lights_wide.jpg" style="width:100%">
-          </div>
+                    <div class="mySlides">
+                        <div class="numbertext">4 / 6</div>
+                        <img src="galeria/img_lights_wide.jpg" style="width:100%">
+                    </div>
 
-          <div class="mySlides">
-            <div class="numbertext">5 / 6</div>
-            <img src="galeria/img_nature_wide.jpg" style="width:100%">
-          </div>
+                    <div class="mySlides">
+                        <div class="numbertext">5 / 6</div>
+                        <img src="galeria/img_nature_wide.jpg" style="width:100%">
+                    </div>
 
-          <div class="mySlides">
-            <div class="numbertext">6 / 6</div>
-            <img src="galeria/img_snow_wide.jpg" style="width:100%">
-          </div>
+                    <div class="mySlides">
+                        <div class="numbertext">6 / 6</div>
+                        <img src="galeria/img_snow_wide.jpg" style="width:100%">
+                    </div>
 
-          <a class="prev" onclick="plusSlides(-1)">❮</a>
-          <a class="next" onclick="plusSlides(1)">❯</a>
+                    <a class="prev" onclick="plusSlides(-1)">❮</a>
+                    <a class="next" onclick="plusSlides(1)">❯</a>
 
-          <div class="caption-container">
-            <p id="caption"></p>
-          </div>
+                    <div class="caption-container">
+                        <p id="caption"></p>
+                    </div>
 
-          <div class="row">
-            <div class="container">
-            <div class="column ">
-              <img class="demo cursor" src="galeria/img_woods.jpg" style="width:100%" onclick="currentSlide(1)" alt="The Woods">
+                    <div class="row">
+                        <div class="container">
+                            <div class="column ">
+                                <img class="demo cursor" src="galeria/img_woods.jpg" style="width:100%" onclick="currentSlide(1)" alt="The Woods">
+                            </div>
+                            <div class="column">
+                                <img class="demo cursor" src="galeria/img_5terre.jpg" style="width:100%" onclick="currentSlide(2)" alt="Cinque Terre">
+                            </div>
+                            <div class="column">
+                                <img class="demo cursor" src="galeria/img_mountains.jpg" style="width:100%" onclick="currentSlide(3)" alt="Mountains and fjords">
+                            </div>
+                            <div class="column">
+                                <img class="demo cursor" src="galeria/img_lights.jpg" style="width:100%" onclick="currentSlide(4)" alt="Northern Lights">
+                            </div>
+                            <div class="column">
+                                <img class="demo cursor" src="galeria/img_nature.jpg" style="width:100%" onclick="currentSlide(5)" alt="Nature and sunrise">
+                            </div>
+                            <div class="column">
+                                <img class="demo cursor" src="galeria/img_snow.jpg" style="width:100%" onclick="currentSlide(6)" alt="Snowy Mountains">
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div class="column">
-              <img class="demo cursor" src="galeria/img_5terre.jpg" style="width:100%" onclick="currentSlide(2)" alt="Cinque Terre">
-            </div>
-            <div class="column">
-              <img class="demo cursor" src="galeria/img_mountains.jpg" style="width:100%" onclick="currentSlide(3)" alt="Mountains and fjords">
-            </div>
-            <div class="column">
-              <img class="demo cursor" src="galeria/img_lights.jpg" style="width:100%" onclick="currentSlide(4)" alt="Northern Lights">
-            </div>
-            <div class="column">
-              <img class="demo cursor" src="galeria/img_nature.jpg" style="width:100%" onclick="currentSlide(5)" alt="Nature and sunrise">
-            </div>
-            <div class="column">
-              <img class="demo cursor" src="galeria/img_snow.jpg" style="width:100%" onclick="currentSlide(6)" alt="Snowy Mountains">
-            </div>
-          </div>
         </div>
-        </div>
-      </div>
     </div>
-</div>
 <?php
 }
 public function footer()
@@ -208,35 +210,35 @@ public function footer()
     ?>
 
 
-        <div class="Conteudo">
-            <footer class="rodape">
-                <br>
-                <div class="col-12">
-                    <div class="row">
-                        <div class="col-2"></div>
-                        <div class="col-8">
-                            &copy; 2019 - Caravan Paradise. Todos os direitos reservados.
-                        </div>
-                        <div class="col-2">
-                            <a class="facebook_directin" href="#">
-                                <img width="40px" class="img-fluid" src="images/facebook_icon.png">
-                            </a>
-                            <a class="instagram_icon" href="#">
-                                <img width="40px" class="img-fluid" src="images/instagram-icon.png">
-                            </a>
-
-                        </div>
+    <div class="Conteudo">
+        <footer class="rodape">
+            <br>
+            <div class="col-12">
+                <div class="row">
+                    <div class="col-2"></div>
+                    <div class="col-8">
+                        &copy; 2019 - Caravan Paradise. Todos os direitos reservados.
+                    </div>
+                    <div class="col-2">
+                        <a class="facebook_directin" href="#">
+                            <img width="40px" class="img-fluid" src="images/facebook_icon.png">
+                        </a>
+                        <a class="instagram_icon" href="#">
+                            <img width="40px" class="img-fluid" src="images/instagram-icon.png">
+                        </a>
 
                     </div>
+
                 </div>
-                <br>
+            </div>
+            <br>
 
-            </footer>
+        </footer>
 
 
-        </div>
+    </div>
 
-    <?php
+<?php
 }
 }
 ?>
