dcdbcccd65f25447100cc2f05c9f5a82edb87533
Jhonatan Luiz <jonatapqt01@gmail.com>
2019-05-27 11:07:56 -0300

    +bugfix

--- site/test.php
+++ site/test.php
@@ -15,105 +15,97 @@
   <link href="css/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
 </head>
 
-
 <style>
-  body {
-    font-family: sans-serif;;
-    margin: 0;
-  }
-.container{
-  font-family: sans-serif;
-}
-  .slideshow {
-    box-sizing: border-box;
-  }
-
-  img {
-    vertical-align: middle;
+  .checkrow {
+    display: -ms-flexbox;
+    /* IE10 */
+    display: flex;
+    -ms-flex-wrap: wrap;
+    /* IE10 */
+    flex-wrap: wrap;
+    margin: 0 -16px;
   }
 
-  /* Position the image container (needed to position the left and right arrows) */
-  .slideimg {
-    position: relative;
+  .col-25 {
+    -ms-flex: 25%;
+    /* IE10 */
+    flex: 25%;
   }
 
-  /* Hide the images by default */
-  .mySlides {
-    display: none;
+  .col-50 {
+    -ms-flex: 50%;
+    /* IE10 */
+    flex: 50%;
   }
 
-  /* Add a pointer when hovering over the thumbnail images */
-  .cursor {
-    cursor: pointer;
+  .col-75 {
+    -ms-flex: 75%;
+    /* IE10 */
+    flex: 75%;
   }
 
-  /* Next & previous buttons */
-  .prev,
-  .next {
-    cursor: pointer;
-    position: absolute;
-    top: 40%;
-    width: auto;
-    padding: 16px;
-    margin-top: -50px;
+  .col-25,
+  .col-50,
+  .col-75 {
+    padding: 0 16px;
+  }
+
+  .check {
+    background-color: #f2f2f2;
+    padding: 5px 20px 15px 20px;
+    border: 1px solid lightgrey;
+    border-radius: 3px;
+  }
+
+  input[type=text] {
+    width: 100%;
+    margin-bottom: 20px;
+    padding: 12px;
+    border: 1px solid #ccc;
+    border-radius: 3px;
+  }
+
+  label {
+    margin-bottom: 10px;
+    display: block;
+  }
+
+  .icon-container {
+    margin-bottom: 20px;
+    padding: 7px 0;
+    font-size: 24px;
+  }
+
+  .btn {
+    background-color: #4CAF50;
     color: white;
-    font-weight: bold;
-    font-size: 20px;
-    border-radius: 0 3px 3px 0;
-    user-select: none;
-    -webkit-user-select: none;
+    padding: 12px;
+    margin: 10px 0;
+    border: none;
+    width: 100%;
+    border-radius: 3px;
+    cursor: pointer;
+    font-size: 17px;
   }
 
-  /* Position the "next button" to the right */
-  .next {
-    right: 0;
-    border-radius: 3px 0 0 3px;
+  .btn:hover {
+    background-color: #45a049;
   }
 
-  /* On hover, add a black background color with a little bit see-through */
-  .prev:hover,
-  .next:hover {
-    background-color: #15bdb1;
-    opacity: 0.5;
+  span.price {
+    float: right;
+    color: grey;
   }
 
-  /* Number text (1/3 etc) */
-  .numbertext {
-    color: #f2f2f2;
-    font-size: 12px;
-    padding: 8px 12px;
-    position: absolute;
-    top: 0;
-  }
+  /* Responsive layout - when the screen is less than 800px wide, make the two columns stack on top of each other instead of next to each other (and change the direction - make the "cart" column go on top) */
+  @media (max-width: 800px) {
+    .checkrow {
+      flex-direction: column-reverse;
+    }
 
-  /* Container for image text */
-  .caption-container{
-    text-align: center;
-    background-color: #15bdb1;;
-    padding: 2px 16px;
-    color: black;
-  }
-
-  .row:after {
-    content: "";
-    display: table;
-    clear: both;
-  }
-
-  /* Six columns side by side */
-  .column {
-    float: left;
-    width: 16.66%;
-  }
-
-  /* Add a transparency effect for thumnbail images */
-  .demo {
-    opacity: 0.6;
-  }
-
-  .active,
-  .demo:hover {
-    opacity: 1;
+    .col-25 {
+      margin-bottom: 20px;
+    }
   }
 </style>
 
@@ -125,164 +117,119 @@
 
   ?>
 
-  <br><br>
-
-  <div class="container_img">
-    <img src="images/porto.jpg" alt="Snow" style="width:100%;">
-    <div class=" container centered">
-      <h3>Caravan Paradise</h3>
-      <input type="text" class="mb-2">
-      <br>
-      <button class="btn btn-primary">Pesquisar</button>
-    </div>
-  </div>
+<br><br><br><br>
 
 
   <div class="container">
 
+    <div class="row checkrow">
+      <div class="col-75">
+        <div class="container check">
+          <form action="/action_page.php">
 
+            <div class="row checkrow">
+              <div class="col-50">
+                <h3>Billing Address</h3>
+                <label for="fname"><i class="fa fa-user"></i> Full Name</label>
+                <input type="text" id="fname" name="firstname" placeholder="John M. Doe">
+                <label for="email"><i class="fa fa-envelope"></i> Email</label>
+                <input type="text" id="email" name="email" placeholder="john@example.com">
+                <label for="adr"><i class="fa fa-address-card-o"></i> Address</label>
+                <input type="text" id="adr" name="address" placeholder="542 W. 15th Street">
+                <label for="city"><i class="fa fa-institution"></i> City</label>
+                <input type="text" id="city" name="city" placeholder="New York">
 
-    <div class="card mt-5">
-      <div class="card-body card">
-        <h2 class="card-title">Caravan Paradise</h2>
-        <p class="card-text">
-          O Caravan Paradise foi idealizado com a proposta de ser, simultaneamente, um sistema de vendas e gerenciamento de viagens e caravanas. O projeto visa facilitar e melhor controlar o funcionamento desse mercado, afim de minimizar possíveis problemas e imprevistos, além de maximizar a experiência dos turistas.
-        </p>
+                <div class="row checkrow">
+                  <div class="col-50">
+                    <label for="state">State</label>
+                    <input type="text" id="state" name="state" placeholder="NY">
+                  </div>
+                  <div class="col-50">
+                    <label for="zip">Zip</label>
+                    <input type="text" id="zip" name="zip" placeholder="10001">
+                  </div>
+                </div>
+              </div>
+
+              <div class="col-50">
+                <h3>Payment</h3>
+                <label for="fname">Accepted Cards</label>
+                <div class="icon-container">
+                  
+                  <i class="fab fa-cc-visa" style="color:navy;"></i>
+                  <i class="fab fa-cc-amex" style="color:blue;"></i>
+                  <i class="fab fa-cc-mastercard" style="color:red;"></i>
+                  <i class="fab fa-cc-discover" style="color:orange;"></i>
+                </div>
+                <label for="cname">Name on Card</label>
+                <input type="text" id="cname" name="cardname" placeholder="John More Doe">
+                <label for="ccnum">Credit card number</label>
+                <input type="text" id="ccnum" name="cardnumber" placeholder="1111-2222-3333-4444">
+                <label for="expmonth">Exp Month</label>
+                <input type="text" id="expmonth" name="expmonth" placeholder="September">
+
+                <div class="row checkrow">
+                  <div class="col-50">
+                    <label for="expyear">Exp Year</label>
+                    <input type="text" id="expyear" name="expyear" placeholder="2018">
+                  </div>
+                  <div class="col-50">
+                    <label for="cvv">CVV</label>
+                    <input type="text" id="cvv" name="cvv" placeholder="352">
+                  </div>
+                </div>
+              </div>
+
+            </div>
+            <label>
+              <input type="checkbox" checked="checked" name="sameadr"> Shipping address same as billing
+            </label>
+            <input type="submit" value="Continue to checkout" class="btn">
+          </form>
+        </div>
+      </div>
+
+      <div class="col-25">
+        <div class="container check">
+          <h4>Cart
+            <span class="price" style="color:black">
+              <i class="fa fa-shopping-cart"></i>
+              <b>4</b>
+            </span>
+          </h4>
+          <p><a href="#">Product 1</a> <span class="price">$15</span></p>
+          <p><a href="#">Product 2</a> <span class="price">$5</span></p>
+          <p><a href="#">Product 3</a> <span class="price">$8</span></p>
+          <p><a href="#">Product 4</a> <span class="price">$2</span></p>
+          <hr>
+          <p>Total <span class="price" style="color:black"><b>$30</b></span></p>
+        </div>
       </div>
     </div>
+
+
   </div>
 
 
-  <br><br>
-  <div class="container">
-    <div class="center">
-
-      <?php
-      $setup->cards(array(1, 1, 1, 1));
-      ?>
 
 
-
-    </div>
-  </div>
   <br><br><br>
 
-  <div>
-    <div class="container">
-      <div class="slideshow">
-        <h2 style="text-align:center">Slideshow Gallery</h2>
 
-        <div class="slideimg">
-          <div class="mySlides">
-            <div class="numbertext">1 / 6</div>
-            <img src="galeria/img_woods_wide.jpg" style="width:100%">
-          </div>
+  <br>
+  <div class="center">
 
-          <div class="mySlides">
-            <div class="numbertext">2 / 6</div>
-            <img src="galeria/img_5terre_wide.jpg" style="width:100%">
-          </div>
+    <?php
+    $setup->footer();
+    ?>
 
-          <div class="mySlides">
-            <div class="numbertext">3 / 6</div>
-            <img src="galeria/img_mountains_wide.jpg" style="width:100%">
-          </div>
-
-          <div class="mySlides">
-            <div class="numbertext">4 / 6</div>
-            <img src="galeria/img_lights_wide.jpg" style="width:100%">
-          </div>
-
-          <div class="mySlides">
-            <div class="numbertext">5 / 6</div>
-            <img src="galeria/img_nature_wide.jpg" style="width:100%">
-          </div>
-
-          <div class="mySlides">
-            <div class="numbertext">6 / 6</div>
-            <img src="galeria/img_snow_wide.jpg" style="width:100%">
-          </div>
-
-          <a class="prev" onclick="plusSlides(-1)">❮</a>
-          <a class="next" onclick="plusSlides(1)">❯</a>
-
-          <div class="caption-container">
-            <p id="caption"></p>
-          </div>
-
-          <div class="row">
-            <div class="container">
-            <div class="column ">
-              <img class="demo cursor" src="galeria/img_woods.jpg" style="width:100%" onclick="currentSlide(1)" alt="The Woods">
-            </div>
-            <div class="column">
-              <img class="demo cursor" src="galeria/img_5terre.jpg" style="width:100%" onclick="currentSlide(2)" alt="Cinque Terre">
-            </div>
-            <div class="column">
-              <img class="demo cursor" src="galeria/img_mountains.jpg" style="width:100%" onclick="currentSlide(3)" alt="Mountains and fjords">
-            </div>
-            <div class="column">
-              <img class="demo cursor" src="galeria/img_lights.jpg" style="width:100%" onclick="currentSlide(4)" alt="Northern Lights">
-            </div>
-            <div class="column">
-              <img class="demo cursor" src="galeria/img_nature.jpg" style="width:100%" onclick="currentSlide(5)" alt="Nature and sunrise">
-            </div>
-            <div class="column">
-              <img class="demo cursor" src="galeria/img_snow.jpg" style="width:100%" onclick="currentSlide(6)" alt="Snowy Mountains">
-            </div>
-          </div>
-        </div>
-        </div>
-      </div>
-    </div>
-    <br>
-    <div class="center">
-
-      <?php
-      $setup->footer();
-      ?>
-
-    </div>
+  </div>
 
 
-    <!-- Bootstrap core JavaScript -->
-    <script src="js/jquery.js"></script>
-    <script src="bootstrap/js/bootstrap.bundle.js"></script>
+  <!-- Bootstrap core JavaScript -->
+  <script src="js/jquery.js"></script>
+  <script src="bootstrap/js/bootstrap.bundle.js"></script>
 
-    <script>
-      var slideIndex = 1;
-      showSlides(slideIndex);
-
-      function plusSlides(n) {
-        showSlides(slideIndex += n);
-      }
-
-      function currentSlide(n) {
-        showSlides(slideIndex = n);
-      }
-
-      function showSlides(n) {
-        var i;
-        var slides = document.getElementsByClassName("mySlides");
-        var dots = document.getElementsByClassName("demo");
-        var captionText = document.getElementById("caption");
-        if (n > slides.length) {
-          slideIndex = 1
-        }
-        if (n < 1) {
-          slideIndex = slides.length
-        }
-        for (i = 0; i < slides.length; i++) {
-          slides[i].style.display = "none";
-        }
-        for (i = 0; i < dots.length; i++) {
-          dots[i].className = dots[i].className.replace(" active", "");
-        }
-        slides[slideIndex - 1].style.display = "block";
-        dots[slideIndex - 1].className += " active";
-        captionText.innerHTML = dots[slideIndex - 1].alt;
-      }
-    </script>
 
 
 </body>
